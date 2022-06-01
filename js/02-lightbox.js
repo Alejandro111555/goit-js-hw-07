5edde83797df11e97bffd5c57654eb6b743b2e64
alejandro111555 <al.mirskoy@gmail.com>
2022-06-01 22:24:17 +0300

    added a second gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryWrapper = document.querySelector('.gallery');
+
+
+const imgList = galleryItems.map(({preview,description,original})=> `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /> </a> `).join('');
+  galleryWrapper.insertAdjacentHTML('beforeend',imgList);
+
+  galleryWrapper.addEventListener('click' , bigImgGallery);
+
+  let gallery = new SimpleLightbox('.gallery a',{
+    captionsData:`alt`,
+    captionDelay:250 
+  });
+
+  function bigImgGallery (event) {
+    if (e.target.nodeName !== "IMG") {
+        return;
+      }
+}
